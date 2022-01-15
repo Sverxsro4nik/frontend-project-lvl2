43ac58b32b4e72675a074aa542d5ff4df38d09ec
Dmitriu Volchenko <Sverxsro4nik@gmail.com>
2022-01-15 22:30:00 +0500

    Create help information to project

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,11 +1,8 @@
 #!/usr/bin/env node
 
 import { Command } from "commander";
-const program = new Command;
+const program = new Command();
 program.version('0.0.1');
 program
   .description('Compares two configuration files and shows a difference.')
-  .option('-V, --version output the version number')
-  .option('-h, --help    output usage information');
-
-program.parse(process.argv);
+  .parse(process.argv);
