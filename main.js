231e4d0750625b468c99194208035c258a9e592e
Dmitriu Volchenko <Sverxsro4nik@gmail.com>
2022-02-07 14:17:38 +0500

    create finall createDiff function

--- main.js
+++ main.js
@@ -6,20 +6,26 @@ const createDiff = (data1, data2) => {
   const keys2 = Object.keys(data2);
   const uniqKeys = _.uniq(_.concat(keys1, keys2));
   const sortedkeys = _.sortBy(uniqKeys);
-  const diffTree = sortedkeys.reduce((acc, key) => {
-    if (_.isObject(data1[key]) && (_.isObject(data2[key]))) {
-      acc[key] = createDiff(data1[key], data2[key]);
-    }
+  const diffTree = sortedkeys.flatMap((key) => {
     if (_.has(data1, key) && !_.has(data2, key)) {
-      acc[key] = 'deleted';
-    } else if (!_.has(data1, key) && _.has(data2, key)) {
-      acc[key] = 'added';
-    } else if (_.has(data1, key) && _.has(data2, key)) {
-      acc[key] = data1[key] === data2[key] ? 'unchanged' : 'changed';
+      return { flag: 'deleted', key, value1: data1[key] };
     }
-    return acc;
-  }, {});
-  console.log(diffTree);
+    if (!_.has(data1, key) && _.has(data2, key)) {
+      return { flag: 'added', key, value1: data2[key] };
+    }
+    if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
+      return { flag: 'object', key, value: createDiff(data1[key], data2[key]) };
+    }
+    if (_.has(data1, key) && _.has(data2, key)) {
+      if (data1[key] !== data2[key]) {
+        return {
+          flag: 'changed', key, value1: data1[key], value2: data2[key],
+        };
+      }
+    }
+    return { flag: 'unchanged', key, value1: data1[key] };
+  });
+  return diffTree;
 };
 
 const genDiff = (filepath1, filepath2) => {
@@ -27,6 +33,7 @@ const genDiff = (filepath1, filepath2) => {
   const data2 = parser(filepath2);
 
   const result = createDiff(data1, data2);
+  console.log(result);
   return result;
 };
 
