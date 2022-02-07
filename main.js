8806ba483c6e3db07e7cc53ebcb1fc8365ff2467
Dmitriu Volchenko <Sverxsro4nik@gmail.com>
2022-02-07 21:47:12 +0500

    create stylish and stringify function

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import parser from './parser.js';
+import stylish from './stylish.js';
 
 const createDiff = (data1, data2) => {
   const keys1 = Object.keys(data1);
@@ -8,22 +9,22 @@ const createDiff = (data1, data2) => {
   const sortedkeys = _.sortBy(uniqKeys);
   const diffTree = sortedkeys.flatMap((key) => {
     if (_.has(data1, key) && !_.has(data2, key)) {
-      return { flag: 'deleted', key, value1: data1[key] };
+      return { flag: 'deleted', dataKey: key, value1: data1[key] };
     }
     if (!_.has(data1, key) && _.has(data2, key)) {
-      return { flag: 'added', key, value1: data2[key] };
+      return { flag: 'added', dataKey: key, value1: data2[key] };
     }
     if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
-      return { flag: 'object', key, value: createDiff(data1[key], data2[key]) };
+      return { flag: 'object', dataKey: key, value1: createDiff(data1[key], data2[key]) };
     }
     if (_.has(data1, key) && _.has(data2, key)) {
       if (data1[key] !== data2[key]) {
         return {
-          flag: 'changed', key, value1: data1[key], value2: data2[key],
+          flag: 'changed', dataKey: key, value1: data1[key], value2: data2[key],
         };
       }
     }
-    return { flag: 'unchanged', key, value1: data1[key] };
+    return { flag: 'unchanged', dataKey: key, value1: data1[key] };
   });
   return diffTree;
 };
@@ -33,8 +34,9 @@ const genDiff = (filepath1, filepath2) => {
   const data2 = parser(filepath2);
 
   const result = createDiff(data1, data2);
-  console.log(result);
-  return result;
+  // console.log(result);
+  const genData = stylish(result);
+  console.log(genData);
 };
 
 export default genDiff;
--- stylish.js
+++ stylish.js
@@ -1,31 +1,40 @@
 import _ from 'lodash';
 
-const stylish = (data1, data2, key) => {
-  if (_.has(data1, key) && !_.has(data2, key)) {
-    return `- ${key}: ${data1[key]}`;
-  } if (_.has(data1, key) && _.has(data2, key) && data1[key] === data2[key]) {
-    return `  ${key}: ${data2[key]}`;
-  } if (_.has(data1, key) && _.has(data2, key) && data1[key] !== data2[key]) {
-    return `- ${key}: ${data1[key]},
-            + ${key}: ${data2[key]}`;
-  }
-  return `+ ${key}: ${data2[key]}`;
-};
-export default stylish;
+const step = (depth, replacer = ' ', spaceCount = 4) => replacer.repeat(spaceCount * depth - 2);
 
-// const result = keys.reduce((acc, key) => {
-//   if (_.has(firstFile, key) && !_.has(secondFile, key)) {
-//     acc.push({ diff: '-', key, value: firstFile[key] });
-//   }
-//   if (_.has(firstFile, key) && _.has(secondFile, key) && firstFile[key] === secondFile[key]) {
-//     acc.push({ diff: ' ', key, value: secondFile[key] });
-//   }
-//   if (_.has(firstFile, key) && _.has(secondFile, key) && firstFile[key] !== secondFile[key]) {
-//     acc.push({ diff: '-', key, value: firstFile[key] });
-//     acc.push({ diff: '+', key, value: secondFile[key] });
-//   }
-//   if (!_.has(firstFile, key) && _.has(secondFile, key)) {
-//     acc.push({ diff: '+', key, value: secondFile[key] });
-//   }
-//   return acc;
-// }, []);
+const stringify = (data, depth) => {
+  if (!_.isObject(data)) {
+    return `${data}`;
+  }
+  const lines = Object
+    .entries(data)
+    .map(([, { flag, dataKey, value1 }]) => `${step(depth + 1)}${flag} ${dataKey}: ${stringify(value1, depth + 1)}`);
+  return [
+    '{',
+    ...lines,
+    '}',
+  ].join('\n');
+};
+
+const stylish = (changedTree) => {
+  const iter = (data, depth) => data.map((node) => {
+    const getNodeValue = (value, flag) => `${step(depth)}${flag} ${node.dataKey}: ${stringify(value, depth)}\n`;
+    switch (node.flag) {
+      case 'added':
+        return getNodeValue(node.value1, '+');
+      case 'deleted':
+        return getNodeValue(node.value1, '-');
+      case 'unchanged':
+        return getNodeValue(node.value1, ' ');
+      case 'changed':
+        return `${getNodeValue(node.value1, '-')}${getNodeValue(node.value2, '+')}`;
+      case 'object':
+        return `${step(depth)} ${node.dataKey}: {\n${iter(node.value1, depth + 1).join('')}${step(depth)} }\n`;
+      default:
+        throw new Error(`Этот тип не определен: ${node.flag}`);
+    }
+  });
+  return `{\n${iter(changedTree, 1).join('')}}`;
+};
+
+export default stylish;
