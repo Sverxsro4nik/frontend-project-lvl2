2416761dd89a999e723d68e930c3578e8390e8cd
Dmitriu Volchenko <Sverxsro4nik@gmail.com>
2022-02-09 15:53:44 +0500

    delete flat file? delete test flat file

--- __tests__/main.test.js
+++ __tests__/main.test.js
@@ -6,35 +6,6 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-test('diff fiel', async () => {
-  const firstFile = getFixturePath('flat-file1.json');
-  const secondFile = getFixturePath('flat-file2.json');
-  expect(diffFile(firstFile, secondFile)).toBe(
-    `{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-}`,
-  );
-});
-
-test('diff yaml file', async () => {
-  const firstFile = getFixturePath('flat-filepath1.yml');
-  const secondFile = getFixturePath('flat-filepath2.yml');
-  expect(diffFile(firstFile, secondFile)).toBe(
-    `{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-}`,
-  );
-});
 test('diff flat json fiel', async () => {
   const firstFile = getFixturePath('file1.json');
   const secondFile = getFixturePath('file2.json');
--- __tests__/parser.test.js
+++ __tests__/parser.test.js
@@ -10,15 +10,14 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-test('parser', async () => {
-  const firstFile = getFixturePath('flat-filepath1.yml');
+test('parser yaml', async () => {
+  const firstFile = getFixturePath('filepath1.yml');
   const needData = load(readFileSync(firstFile, 'utf-8'));
-  console.log(needData);
   expect(parser(firstFile)).toStrictEqual(needData);
 });
 
 test('parser json', async () => {
-  const firstFile = getFixturePath('flat-file1.json');
+  const firstFile = getFixturePath('file1.json');
   const needData = JSON.parse(readFileSync(firstFile, 'utf-8'));
   expect(parser(firstFile)).toStrictEqual(needData);
 });
