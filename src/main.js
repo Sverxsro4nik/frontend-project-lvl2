c080a6b373f4381c8f6ecaa7991aff231b63a261
Dmitriu Volchenko <Sverxsro4nik@gmail.com>
2022-02-11 15:12:34 +0500

    delete let in parser function

--- src/main.js
+++ src/main.js
@@ -2,10 +2,9 @@ import parser from './parser.js';
 import formatData from './formatters/index.js';
 import createDiff from './createDiff.js';
 
-const genDiff = (filepath1, filepath2, format = 'json') => {
+const genDiff = (filepath1, filepath2, format = 'stylish') => {
   const data1 = parser(filepath1);
   const data2 = parser(filepath2);
-
   const result = createDiff(data1, data2);
   const genData = formatData(result, format);
   return genData;
--- src/parser.js
+++ src/parser.js
@@ -6,14 +6,7 @@ import { readFileSync } from 'fs';
 const parser = (file) => {
   const data = readFileSync(path.resolve(file) || cwd(file), 'utf-8');
   const fileExtension = path.extname(file);
-  let resultData;
-  if (fileExtension === '.json') {
-    resultData = JSON.parse(data);
-  }
-  if (fileExtension === '.yml' || fileExtension === '.yaml') {
-    resultData = load(data);
-  }
-  return resultData;
+  return fileExtension === '.json' ? JSON.parse(data) : load(data);
 };
 
 export default parser;
