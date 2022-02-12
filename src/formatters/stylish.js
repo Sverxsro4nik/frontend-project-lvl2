ccdc8ffe1f492df752eb0218cb9f048e9458c72f
Dmitriu Volchenko <Sverxsro4nik@gmail.com>
2022-02-12 15:51:09 +0500

    rename createString function

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -18,16 +18,16 @@ const stringify = (data, depth) => {
 
 const stylish = (changedTree) => {
   const iter = (data, depth) => data.map((node) => {
-    const getNodeValue = (value, flag) => `${step(depth)}${flag} ${node.key}: ${stringify(value, depth)}\n`;
+    const createString = (value, flag) => `${step(depth)}${flag} ${node.key}: ${stringify(value, depth)}\n`;
     switch (node.flag) {
       case 'added':
-        return getNodeValue(node.value, '+');
+        return createString(node.value, '+');
       case 'deleted':
-        return getNodeValue(node.value, '-');
+        return createString(node.value, '-');
       case 'unchanged':
-        return getNodeValue(node.value, ' ');
+        return createString(node.value, ' ');
       case 'changed':
-        return `${getNodeValue(node.value1, '-')}${getNodeValue(node.value2, '+')}`;
+        return `${createString(node.value1, '-')}${createString(node.value2, '+')}`;
       case 'object':
         return `${step(depth)}  ${node.key}: {\n${iter(node.child, depth + 1).join('')}${step(depth)}  }\n`;
       default:
