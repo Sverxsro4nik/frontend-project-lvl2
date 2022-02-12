7df5ec2e84c304153fb3d07c5a7d0218e98e943a
Dmitriu Volchenko <Sverxsro4nik@gmail.com>
2022-02-12 15:19:11 +0500

    update tests create result file

--- __tests__/main.test.js
+++ __tests__/main.test.js
@@ -1,133 +1,37 @@
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
+import { readFileSync } from 'fs';
+import { expect, test } from '@jest/globals';
 import diffFile from '../src/main.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (dataPath) => readFileSync(dataPath, 'utf-8');
 
 test('diff flat json fiel', async () => {
   const firstFile = getFixturePath('file1.json');
   const secondFile = getFixturePath('file2.json');
-  expect(diffFile(firstFile, secondFile, 'stylish')).toBe(
-    `{
-    common: {
-      + follow: false
-        setting1: Value 1
-      - setting2: 200
-      - setting3: true
-      + setting3: null
-      + setting4: blah blah
-      + setting5: {
-            key5: value5
-        }
-        setting6: {
-            doge: {
-              - wow: 
-              + wow: so much
-            }
-            key: value
-          + ops: vops
-        }
-    }
-    group1: {
-      - baz: bas
-      + baz: bars
-        foo: bar
-      - nest: {
-            key: value
-        }
-      + nest: str
-    }
-  - group2: {
-        abc: 12345
-        deep: {
-            id: 45
-        }
-    }
-  + group3: {
-        deep: {
-            id: {
-                number: 45
-            }
-        }
-        fee: 100500
-    }
-}`,
-  );
+  const result = getFixturePath('needStylishData.txt');
+  expect(diffFile(firstFile, secondFile, 'stylish')).toBe(readFile(result));
 });
 test('diff flat yaml file', async () => {
   const firstFile = getFixturePath('filepath1.yml');
   const secondFile = getFixturePath('filepath2.yml');
-  expect(diffFile(firstFile, secondFile, 'stylish')).toBe(
-    `{
-    common: {
-      + follow: false
-        setting1: Value 1
-      - setting2: 200
-      - setting3: true
-      + setting3: null
-      + setting4: blah blah
-      + setting5: {
-            key5: value5
-        }
-        setting6: {
-            doge: {
-              - wow: 
-              + wow: so much
-            }
-            key: value
-          + ops: vops
-        }
-    }
-    group1: {
-      - baz: bas
-      + baz: bars
-        foo: bar
-      - nest: {
-            key: value
-        }
-      + nest: str
-    }
-  - group2: {
-        abc: 12345
-        deep: {
-            id: 45
-        }
-    }
-  + group3: {
-        deep: {
-            id: {
-                number: 45
-            }
-        }
-        fee: 100500
-    }
-}`,
-  );
+  const result = getFixturePath('needStylishData.txt');
+  expect(diffFile(firstFile, secondFile, 'stylish')).toBe(readFile(result));
 });
 
 test('flat format', async () => {
   const firstFile = getFixturePath('filepath1.yml');
   const secondFile = getFixturePath('filepath2.yml');
-  expect(diffFile(firstFile, secondFile, 'plain')).toBe(
-    `Property 'common.follow' was added with value: false
-Property 'common.setting2' was removed
-Property 'common.setting3' was updated. From true to null
-Property 'common.setting4' was added with value: 'blah blah'
-Property 'common.setting5' was added with value: [complex value]
-Property 'common.setting6.doge.wow' was updated. From '' to 'so much'
-Property 'common.setting6.ops' was added with value: 'vops'
-Property 'group1.baz' was updated. From 'bas' to 'bars'
-Property 'group1.nest' was updated. From [complex value] to 'str'
-Property 'group2' was removed
-Property 'group3' was added with value: [complex value]`,
-  );
+  const result = getFixturePath('needPlainData.txt');
+  expect(diffFile(firstFile, secondFile, 'plain')).toBe(readFile(result));
 });
 
 test('json format', async () => {
-  const firstFile = getFixturePath('file1.json');
-  const secondFile = getFixturePath('file2.json');
-  const jsonDiff = diffFile(firstFile, secondFile, 'json');
-  expect(diffFile(firstFile, secondFile, 'json')).toStrictEqual(jsonDiff);
+  const firstFile = getFixturePath('filepath1.yml');
+  const secondFile = getFixturePath('filepath2.yml');
+  const result = getFixturePath('resultJson.txt');
+  expect(diffFile(firstFile, secondFile, 'json')).toStrictEqual(readFile(result));
 });
