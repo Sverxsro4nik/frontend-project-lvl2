e295f740affe59b7b2e2f8ce60859791be0780c7
Dmitriu Volchenko <Sverxsro4nik@gmail.com>
2022-02-15 21:17:50 +0500

    create one test for all check

--- __tests__/main.test.js
+++ __tests__/main.test.js
@@ -1,37 +1,26 @@
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
 import { readFileSync } from 'fs';
-import { expect, test } from '@jest/globals';
-import diffFile from '../index.js';
+import genDiff from '../index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (dataPath) => readFileSync(dataPath, 'utf-8');
 
-test('diff flat json fiel', async () => {
-  const firstFile = getFixturePath('file1.json');
-  const secondFile = getFixturePath('file2.json');
-  const result = getFixturePath('needStylishData.txt');
-  expect(diffFile(firstFile, secondFile, 'stylish')).toBe(readFile(result));
-});
-test('diff flat yaml file', async () => {
-  const firstFile = getFixturePath('filepath1.yml');
-  const secondFile = getFixturePath('filepath2.yml');
-  const result = getFixturePath('needStylishData.txt');
-  expect(diffFile(firstFile, secondFile, 'stylish')).toBe(readFile(result));
-});
+const testFields = [
+  ['file1.json', 'file2.json', 'needStylishData.txt', 'stylish'],
+  ['filepath1.yml', 'filepath2.yml', 'needStylishData.txt', 'stylish'],
+  ['file1.json', 'file2.json', 'needPlainData.txt', 'plain'],
+  ['filepath1.yml', 'filepath2.yml', 'needPlainData.txt', 'plain'],
+  ['file1.json', 'file2.json', 'resultJson.txt', 'json'],
+  ['filepath1.yml', 'filepath2.yml', 'resultJson.txt', 'json'],
+];
 
-test('flat format', async () => {
-  const firstFile = getFixturePath('filepath1.yml');
-  const secondFile = getFixturePath('filepath2.yml');
-  const result = getFixturePath('needPlainData.txt');
-  expect(diffFile(firstFile, secondFile, 'plain')).toBe(readFile(result));
-});
-
-test('json format', async () => {
-  const firstFile = getFixturePath('filepath1.yml');
-  const secondFile = getFixturePath('filepath2.yml');
-  const result = getFixturePath('resultJson.txt');
-  expect(diffFile(firstFile, secondFile, 'json')).toStrictEqual(readFile(result));
+test.each(testFields)('Comparison %s and %s and check the identity of the received %s', (firstFile, secondFile, needResult, format) => {
+  const firstData = getFixturePath(firstFile);
+  const secondData = getFixturePath(secondFile);
+  const resultData = readFile(getFixturePath(needResult));
+  const result = genDiff(firstData, secondData, format);
+  expect(result).toEqual(resultData);
 });
