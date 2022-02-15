f954b76993cfad6e7035ace3bc0df61a9cdb5b20
Dmitriu Volchenko <Sverxsro4nik@gmail.com>
2022-02-15 21:40:20 +0500

    update parser test

--- __tests__/parser.test.js
+++ __tests__/parser.test.js
@@ -1,6 +1,5 @@
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
-import { expect, test } from '@jest/globals';
 import { load } from 'js-yaml';
 import { readFileSync } from 'fs';
 
@@ -12,16 +11,18 @@ const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', 
 
 const readFile = (file) => readFileSync(file, 'utf-8');
 
-test('parser yaml', async () => {
+test('parser yaml', () => {
   const firstFile = getFixturePath('filepath1.yml');
+  const extension = path.extname(firstFile);
   const data = readFile(firstFile);
   const needData = load(readFileSync(firstFile, 'utf-8'));
-  expect(parser(data)).toStrictEqual(needData);
+  expect(parser(data, extension)).toStrictEqual(needData);
 });
 
-test('parser json', async () => {
+test('parser json', () => {
   const firstFile = getFixturePath('file1.json');
+  const extension = path.extname(firstFile);
   const data = readFile(firstFile);
   const needData = JSON.parse(readFileSync(firstFile, 'utf-8'));
-  expect(parser(data)).toStrictEqual(needData);
+  expect(parser(data, extension)).toStrictEqual(needData);
 });
