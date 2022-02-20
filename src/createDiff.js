eed582061f9b8637eddf17602188dedd30d5098b
Dmitriu Volchenko <Sverxsro4nik@gmail.com>
2022-02-20 14:08:22 +0500

    delete parser test, update createDiff logic

--- src/createDiff.js
+++ src/createDiff.js
@@ -6,25 +6,21 @@ const createDiff = (data1, data2) => {
   const uniqKeys = _.uniq(_.concat(keys1, keys2));
   const sortedkeys = _.sortBy(uniqKeys);
   const diffTree = sortedkeys.flatMap((key) => {
-    const valueData1 = data1[key];
-    const valueData2 = data2[key];
     if (_.has(data1, key) && !_.has(data2, key)) {
-      return { flag: 'deleted', key, value: valueData1 };
+      return { flag: 'deleted', key, value: data1[key] };
     }
     if (!_.has(data1, key) && _.has(data2, key)) {
-      return { flag: 'added', key, value: valueData2 };
+      return { flag: 'added', key, value: data2[key] };
     }
-    if (_.isPlainObject(valueData1) && _.isPlainObject(valueData2)) {
-      return { flag: 'object', key, child: createDiff(valueData1, valueData2) };
+    if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
+      return { flag: 'object', key, child: createDiff(data1[key], data2[key]) };
     }
-    if (_.has(data1, key) && _.has(data2, key)) {
-      if (valueData1 !== valueData2) {
-        return {
-          flag: 'changed', key, value1: valueData1, value2: valueData2,
-        };
-      }
+    if (!_.isEqual(data1[key], data2[key])) {
+      return {
+        flag: 'changed', key, value1: data1[key], value2: data2[key],
+      };
     }
-    return { flag: 'unchanged', key, value: valueData1 };
+    return { flag: 'unchanged', key, value: data1[key] };
   });
   return diffTree;
 };
